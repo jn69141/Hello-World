9f07ae3b002f5768a4b1c39d9c717889b01fa26e
jn69141 <69649822+jn69141@users.noreply.github.com>
2020-08-13 12:47:42 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Jennifer Ngo");
+		System.out.println("I like to play league and valorant");
+		System.out.println("But my wifi is not so great");
+		System.out.println("I strongly dislike eating squid");
+		System.out.println("I want to be rich and retire early");
+	}
+}
